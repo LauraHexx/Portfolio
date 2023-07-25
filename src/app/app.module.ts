1f6e85506d0db90069658878204b1f3a7694c719
LauraHexx <l.hesidenz97@gmail.com>
2023-07-25 16:37:08 +0200

    i am section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { IntroductionComponent } from './introduction/introduction.component';
+import { IAmComponent } from './i-am/i-am.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    IntroductionComponent,
-    AboutMeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, IAmComponent, AboutMeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
