c2a367439d3be46c1f11ec603e7665af52b8d7eb
LauraHexx <l.hesidenz97@gmail.com>
2023-07-28 10:06:08 +0200

    bg form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { IAmComponent } from './i-am/i-am.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SingleProjectComponent } from './single-project/single-project.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, IAmComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  declarations: [AppComponent, HeaderComponent, IAmComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SingleProjectComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
