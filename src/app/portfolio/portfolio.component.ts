b35ab9d695e653ac97dd0274e10f089ae7ca85fb
LauraHexx <l.hesidenz97@gmail.com>
2023-08-01 20:12:33 +0200

    start with single projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      title: 'Join',
+      skills: ['javaScript', 'HTML', 'CSS'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      img: 'join.png',
+    },
+    {
+      title: 'El Pollo Loco',
+      skills: ['javaScript', 'HTML', 'CSS'],
+      description:
+        'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and bottles to fight against the killer chicken.',
+      img: 'el-pollo-loco.png',
+    },
+    {
+      title: 'Portfolio',
+      skills: ['Angular ', 'Typescript', 'HTML', 'CSS'],
+      description:
+        'Welcome to my portfolio page! I am excited to share with you some of the projects I have worked on using Angular, TypeScript, HTML, and CSS',
+      img: 'el-pollo-loco.png',
+    },
+  ];
 }
--- src/app/single-project/single-project.component.ts
+++ src/app/single-project/single-project.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-project',
   templateUrl: './single-project.component.html',
-  styleUrls: ['./single-project.component.scss']
+  styleUrls: ['./single-project.component.scss'],
 })
 export class SingleProjectComponent {
-
+  @Input() title: string;
+  @Input() skills: Array<string>;
+  @Input() description: string;
+  @Input() img: string;
 }
