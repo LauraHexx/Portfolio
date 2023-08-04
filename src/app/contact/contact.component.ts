d8571073e96e0e0a3e848235f478ba2f0803aebb
LauraHexx <l.hesidenz97@gmail.com>
2023-08-04 12:51:16 +0200

    change image on hover

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  isHovered: boolean = false;
 
+  onMouseOver(): void {
+    this.isHovered = true;
+  }
+
+  onMouseOut(): void {
+    this.isHovered = false;
+  }
 }
