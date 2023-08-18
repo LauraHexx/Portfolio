2aff98ded8b314e2ae333ae7f26810701c79ae4f
LauraHexx <l.hesidenz97@gmail.com>
2023-08-18 16:41:57 +0200

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,7 @@ import { FormComponent } from './form/form.component';
     GreetingComponent,
     FormComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
+  @ViewChild('form', { static: false }) form!: HTMLFormElement;
+  @ViewChild('nameField', { static: false })
+  nameField!: ElementRef<HTMLInputElement>;
+  @ViewChild('emailField', { static: false })
+  emailField!: ElementRef<HTMLInputElement>;
+  @ViewChild('messageField', { static: false })
+  messageField!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('sendButton', { static: false })
+  sendButton!: ElementRef<HTMLButtonElement>;
 
+  async sendMail() {
+    console.log(this.form);
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+    //TODO: Animation
+
+    let formData = new FormData();
+    formData.append('name', nameField.value);
+    formData.append('email', emailField.value);
+    formData.append('message', messageField.value);
+
+    //senden
+    await fetch(
+      'https://laura-hesidenz.developerakademie.net/send_mail/send_mail.php',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    //TODO: NAchricht gesendet
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
+  }
 }
