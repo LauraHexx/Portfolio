b620acbaceed88ed967bbf85fc04b04d3fdb8860
LauraHexx <l.hesidenz97@gmail.com>
2023-08-18 12:16:55 +0200

    style changes

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-greeting',
   templateUrl: './greeting.component.html',
-  styleUrls: ['./greeting.component.scss']
+  styleUrls: ['./greeting.component.scss'],
 })
-export class GreetingComponent {
-
-}
+export class GreetingComponent {}
