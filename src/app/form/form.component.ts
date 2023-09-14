14a7c5c5ed3b9348e766241e884c386d756ce3df
LauraHexx <l.hesidenz97@gmail.com>
2023-09-14 19:16:21 +0200

    form changes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,21 +7,22 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements AfterViewInit {
-  @ViewChild('form', { static: false }) form: HTMLFormElement;
-  @ViewChild('nameFieldElement', { static: false })
-  nameFieldElementRef: ElementRef;
-  @ViewChild('emailFieldElement', { static: false })
-  emailFieldElementRef: ElementRef;
-  @ViewChild('messageFieldElement', { static: false })
-  messageFieldElementRef: ElementRef;
-  @ViewChild('sendButton', { static: false })
-  sendButtonRef: ElementRef;
-
   nameField;
   emailField;
   messageField;
   sendButton;
   emailWasSent = false;
+  isLoading = false;
+
+  @ViewChild('sendButton', { static: false })
+  sendButtonRef: ElementRef;
+
+  ngAfterViewInit() {
+    this.nameField = this.contactForm.get('nameForm');
+    this.emailField = this.contactForm.get('emailForm');
+    this.messageField = this.contactForm.get('messageForm');
+    this.sendButton = this.sendButtonRef.nativeElement;
+  }
 
   contactForm = new FormGroup({
     nameForm: new FormControl('', [
@@ -35,37 +36,20 @@ export class FormComponent implements AfterViewInit {
     ]),
   });
 
-  ngAfterViewInit() {
-    this.nameField = this.nameFieldElementRef.nativeElement;
-    this.emailField = this.emailFieldElementRef.nativeElement;
-    this.messageField = this.messageFieldElementRef.nativeElement;
-    this.sendButton = this.sendButtonRef.nativeElement;
-  }
+  async sendingMail() {
+    this.contactForm.disable();
+    this.isLoading = true;
 
-  async sendMail() {
-    this.disableForm();
-    // this.checkData();
     let formData = this.getData();
     await this.sendData(formData);
-    this.resetForm();
-    this.enableForm();
-    this.showSucessMessage();
-  }
 
-  disableForm() {
-    this.nameField.disabled = true;
-    this.emailField.disabled = true;
-    this.messageField.disabled = true;
-    this.sendButton.disabled = true;
-  }
+    await this.showSucessMessage();
+    this.isLoading = false;
+    console.log(this.isLoading);
 
-  checkData() {
-    //this.checkNameField();
-    //this.checkEmailField();
-    //this.checkMessageField();
+    this.contactForm.enable();
+    this.contactForm.reset();
   }
-
-  checkNameField() {}
 
   getData() {
     let formData = new FormData();
@@ -76,7 +60,7 @@ export class FormComponent implements AfterViewInit {
   }
 
   async sendData(formData) {
-    fetch(
+    await fetch(
       'https://laura-hesidenz.developerakademie.net/send_mail/send_mail.php',
       {
         method: 'post',
@@ -85,24 +69,42 @@ export class FormComponent implements AfterViewInit {
     );
   }
 
-  enableForm() {
-    this.nameField.disabled = false;
-    this.emailField.disabled = false;
-    this.messageField.disabled = false;
-    this.sendButton.disabled = false;
-  }
-
-  resetForm() {
-    this.nameField.value = '';
-    this.emailField.value = '';
-    this.messageField.value = '';
-  }
-
-  showSucessMessage() {
+  async showSucessMessage() {
     this.emailWasSent = true;
 
     setTimeout(() => {
       this.emailWasSent = false;
     }, 3000);
   }
+
+  /*
+
+ 
+
+ 
+  async sendMail() {
+    this.disableForm();
+    // this.checkData();
+    let formData = this.getData();
+    await this.sendData(formData);
+    this.resetForm();
+    this.enableForm();
+    this.showSucessMessage();
+  }
+
+
+
+
+  checkData() {
+    //this.checkNameField();
+    //this.checkEmailField();
+    //this.checkMessageField();
+  }
+
+  
+
+
+ 
+
+  */
 }
