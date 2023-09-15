f016ad80b2df3510331b67b005316a751af9131c
LauraHexx <l.hesidenz97@gmail.com>
2023-09-15 10:14:34 +0200

    skills schleife

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -5,4 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss'],
 })
-export class MySkillsComponent {}
+export class MySkillsComponent {
+  skills = [
+    {
+      name: 'Angular',
+      image: 'angular.png',
+    },
+    {
+      name: 'TypeScript',
+      image: 'typescript.png',
+    },
+    {
+      name: 'JavaScript',
+      image: 'javaScript.png',
+    },
+    {
+      name: 'HTML',
+      image: 'html.png',
+    },
+    {
+      name: 'Firebase',
+      image: 'firebase.png',
+    },
+    {
+      imnameage: 'Git',
+      image: 'git.png',
+    },
+    {
+      name: 'CSS',
+      image: 'css.png',
+    },
+    {
+      name: 'Rest-Api',
+      image: 'api.png',
+    },
+    {
+      name: 'Scrum',
+      image: 'scrum.png',
+    },
+    {
+      name: 'Material Design',
+      image: 'material-design.png',
+    },
+  ];
+}
